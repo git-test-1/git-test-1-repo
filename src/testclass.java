1dd58ba416bb74664a4237d028786eeba5078935
Colin <colinreadman@tiscali.co.uk>
2014-12-31 09:25:43 +0000

    1st commit of java_test_project

---
+++ src/testclass.java
@@ -0,0 +1,33 @@
+
+public class testclass {
+
+	private String [] [] product;
+	private int quantity;
+
+	/**
+	 * 
+	 */
+	public testclass() {
+		// TODO Auto-generated constructor stub
+		quantity=0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public void setQuantity(Integer i) {
+	
+		quantity=i;
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public Integer getQuantity() {
+	
+		return quantity;
+		
+	}	
+
+}
